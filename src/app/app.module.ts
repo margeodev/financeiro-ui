1a1a58f4ea166887b1e760629ee363bc0d72d24e
marcio <margeodev@gmail.com>
2019-03-23 17:43:14 -0300

    Criação do projeto, criação das pesquisas de pessoas e lançamentos, criação da barra de navegação

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { ButtonModule }    from 'primeng/components/button/button';
+import { TableModule }     from 'primeng/components/table/table';
+import { TooltipModule }         from 'primeng/components/tooltip/tooltip';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent,
+    PessoasPesquisaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
